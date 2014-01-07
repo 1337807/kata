ad7330f206e56ce5b263c2f181cbf69be3caf444
Jonan Scheffler <jonanscheffler@gmail.com>
2014-01-06 22:29:32 -0800

    Coin piles.

--- 5_coin_denomination/main.rb
+++ 5_coin_denomination/main.rb
@@ -18,3 +18,26 @@ class CurrencyTest < Test::Unit::TestCase
     assert_equal [1, 5, 10 , 25, 50, 100], @currency.denominations
   end
 end
+
+class CoinStacker
+  def coin_piles_for_amount_in_denominations(amount, denominations)
+    sorted = denominations.sort.reverse
+
+    max = (amount / denomination)
+    max.times do |count|
+    end
+  end
+end
+
+class CoinStackerTest < Test::Unit::TestCase
+  def setup
+    @stacker = CoinStacker.new
+    @currency = Currency.new
+  end
+
+  def _test_coin_piles_for_currency
+    @currency.parse_denominations('1 5')
+    result = @stacker.coin_piles_for_amount_in_denominations(10, @currency.denominations)
+    assert_equal [[[1, 5], [5, 1], [5, 2], [1, 10]], result
+  end
+end
