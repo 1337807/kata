cba75739b71b1b531582606b9d5b18f118fcca89
Jonan Scheffler <jonanscheffler@gmail.com>
2014-01-08 23:36:45 -0800

    Test touchdowns are valid.

--- 6_sport_points/main.rb
+++ 6_sport_points/main.rb
@@ -1,12 +1,18 @@
 require 'test/unit'
 
 class ScoreValidator
+  attr_accessor :point_types
+
   def score_is_divisible_by_number_in_set?(score, set)
     set.each do |number|
       return true if score % number == 0
     end
 
     false
+  end
+
+  def score_is_valid?(score)
+    return true if score_is_divisible_by_number_in_set?(score, point_types.keys)
   end
 end
 
@@ -29,5 +35,11 @@ class ScoreValidatorTest < Test::Unit::TestCase
       result = @validator.score_is_divisible_by_number_in_set?(score, set)
       refute result
     end
+  end
+
+  def test_score_is_valid_returns_true_for_touchdown_alone
+    @validator.point_types = { 6 => 'touchdown' }
+    result = @validator.score_is_valid?(6)
+    assert result
   end
 end
