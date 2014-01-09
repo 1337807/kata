6afbb70b335adc6eba95011ec3e3de3d67bc70ae
Jonan Scheffler <jonanscheffler@gmail.com>
2014-01-08 23:28:14 -0800

    Test if a number is divisible by any one of a set of numbers.

---
+++ 6_sport_points/main.rb
@@ -0,0 +1,25 @@
+require 'test/unit'
+
+class ScoreValidator
+  def score_is_divisible_by_number_in_set?(score, set)
+    set.each do |number|
+      return true if score % number == 0
+    end
+
+    false
+  end
+end
+
+class ScoreValidatorTest < Test::Unit::TestCase
+  def setup
+    @validator = ScoreValidator.new
+  end
+
+  def test_score_is_divisible_by_number_in_set?
+    [4, 5, 9, 18].each do |score|
+      set = [2, 5, 9]
+      result = @validator.score_is_divisible_by_number_in_set?(score, set)
+      assert result
+    end
+  end
+end
