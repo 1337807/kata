9855e78386c6cbd45952416e901bf9ac1e746cc9
Jonan Scheffler <jonanscheffler@gmail.com>
2014-01-09 21:22:37 -0800

    Emptiness is not sand, and block types will find the emptiness inside you.

--- 7_falling_sand/main.rb
+++ 7_falling_sand/main.rb
@@ -1,8 +1,18 @@
 require 'test/unit'
 
 class Sandscape
+  BLOCK_TYPES = {
+    '.' => 'sand',
+    '#' => 'stone',
+    ' ' => 'empty'
+  }
+
   def is_sand?(block)
     block == '.'
+  end
+
+  def block_type(block)
+    BLOCK_TYPES[block]
   end
 end
 
@@ -12,7 +22,14 @@ class SandscapeTest < Test::Unit::TestCase
   end
 
   def test_is_sand_returns_true_for_dot
-    result = @sandscape.is_sand? '.'
-    assert result
+    assert @sandscape.is_sand? '.'
+  end
+
+  def test_is_sand_returns_false_for_empty
+    refute @sandscape.is_sand? ' '
+  end
+
+  def test_block_type
+    assert_equal 'stone', @sandscape.block_type('#')
   end
 end
