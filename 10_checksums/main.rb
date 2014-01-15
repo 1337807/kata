c79e59ce69fd77bd619b97f480782d9e2a84438d
Jonan Scheffler <jonanscheffler@gmail.com>
2014-01-14 23:44:19 -0800

    Notes and links in case I come back someday.

--- 10_checksums/main.rb
+++ 10_checksums/main.rb
@@ -24,4 +24,13 @@ class ChecksumTest < Test::Unit::TestCase
     result = @checksum.binary_blocks("Rad")
     assert_equal ["10010101", "10111111", "01110110", "00011101", "110"], result
   end
+
+  def test_compute
+    # for each index calculate sum1 and sum2 where:
+    # sum1 = sum1 + block_at[index] % 255
+    # sum2 = sum2 + sum1 % 255
+    # 
+    # then combine sum1 and sum2 (maybe instead of sum2 use sum2 * 256?)
+    # http://en.wikipedia.org/wiki/Fletcher%27s_checksum
+  end
 end
