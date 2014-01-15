ee5f1166cbf2ac26e0cd3593daef35afada6f51a
Jonan Scheffler <jonanscheffler@gmail.com>
2014-01-14 23:30:15 -0800

    Split word into 8 bit blocks.

--- 10_checksums/main.rb
+++ 10_checksums/main.rb
@@ -4,6 +4,10 @@ class Checksum
   def binary(word)
     "Jonan".bytes.map { |letter| letter.to_s(2) }.join
   end
+
+  def binary_blocks(word)
+    binary(word).scan(/.{1,#{8}}/)
+  end
 end
 
 class ChecksumTest < Test::Unit::TestCase
@@ -14,5 +18,10 @@ class ChecksumTest < Test::Unit::TestCase
   def test_binary_converts_given_string_to_binary
     result = @checksum.binary("Jonan")
     assert_equal '10010101101111110111011000011101110', result
+  end
+
+  def test_binary_blocks_converts_given_string_into_eight_bit_blocks
+    result = @checksum.binary_blocks("Rad")
+    assert_equal ["10010101", "10111111", "01110110", "00011101", "110"], result
   end
 end
