90518b06b365283acc097e9fd8a12202a418b506
Jonan Scheffler <jonanscheffler@gmail.com>
2014-02-02 22:41:45 -0800

    Courses average grades for all their students.

--- 15_student_management/main.rb
+++ 15_student_management/main.rb
@@ -1,10 +1,35 @@
 require 'test/unit'
 
+class Course
+  attr_accessor :students
+
+  def average_grade
+    self.students.map(&:average_grade).inject(&:+) / self.students.length
+  end
+end
+
 class Student
   attr_accessor :grades
 
   def average_grade
     self.grades.inject(&:+) / self.grades.length
+  end
+end
+
+class CourseTest < Test::Unit::TestCase
+  def setup
+    @course = Course.new
+  end
+
+  def test_average_reports_the_average_of_all_student_grades
+    students = [
+      Student.new.tap { |s| s.grades = [1, 2, 3] },
+      Student.new.tap { |s| s.grades = [2, 3, 4] },
+      Student.new.tap { |s| s.grades = [3, 4, 5] }
+    ]
+    @course.students = students
+
+    assert_equal 3, @course.average_grade
   end
 end
 
